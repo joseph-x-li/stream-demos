5e583f42a699d58d2a61bab7ce7e31aab423a796
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2021-01-18 16:16:06 -0800

    audj resolution

--- mapreduce/streamer.py
+++ mapreduce/streamer.py
@@ -2,8 +2,8 @@ import pystreaming, cv2, zmq, time
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 stream = pystreaming.Streamer(zmq.Context(), "tcp://*:5555", mapreduce=True)
 stream.start()
