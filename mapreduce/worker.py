1d4962a63a7a36728eaf1eab70da2f8bf7b11ee0
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2021-01-18 16:14:01 -0800

    Finish worker code

--- mapreduce/worker.py
+++ mapreduce/worker.py
@@ -1,26 +1,30 @@
 import pystreaming as ps
 import zmq
 from models import loadmodel
+import cv2
+import jetson.utils
 
 def main():
 
-    model = loadmodel()
+    net = loadmodel()
     
     def func(frame):
         """Return a list of detected objects 
 
         Args:
-            frame (): Image frame of size ()
+            frame (np.ndarray): Image frame. Will be resized to 480 width x 320 height
         """
-        nonlocal model
-        assert 
-        if frame.shape != (, 3):
-            frame = frame[?,?,:]
-        
-        return model(frame)
+        nonlocal net
+
+        frame = cv2.resize(frame, (480, 320))
+        frame = jetson.utils.cudaFromNumpy(frame)
+        detections = net.Detect(frame)
+        ret = [(d.ClassID, d.Top, d.Left, d.Right, d.Right) for d in detections]
+        print(ret)
+        return ret
         
     
-    worker = ps.Worker(zmq.Context(), "tcp://172.16.0.4:5555", "tcp://172.16.0.25:5555")
+    worker = ps.Worker(zmq.Context(), "tcp://172.16.0.4:5555", "tcp://172.16.0.25:5556")
     worker.run(func)
     
 if __name__ == '__main__':
